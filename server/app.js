58a52811b44a497bdd1c3fbc07207d7b49dd371b
VibhorR <vibhorrawat95@gmail.com>
2023-08-03 10:08:49 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,26 +14,9 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-// // Health check endpoint
-// app.get("/health", (req, res) => {
-//   const dbStatus =
-//     mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
-
-//   res.status(200).json({
-//     server: "Running",
-//     database: dbStatus,
-//   });
-// });
-
 app.use("/", authRouter);
 app.use("/products", productRouter);
 app.use("/comments", commentRouter);
-// app.use((req, res, next) => {
-//   const error = new Error("Not found");
-//   error.status = 404;
-//   next(error);
-// });
 
 // Error handler middleware
 app.use((err, req, res, next) => {
